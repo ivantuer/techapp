ae10485d314876105126030603355187f806e750
Ivan Hrynevytskyy <ivantuer@gmail.com>
2018-11-03 13:22:17 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/techapp/precache-manifest.eec19b3f09d3cb263bc7b5bade890917.js"
+  "/techapp/precache-manifest.38064672133b1a6c29f0cff14e07b0f5.js"
 );
 
 workbox.clientsClaim();
